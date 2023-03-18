54bbcb980994fa701a3668095bc816ac6d22a792
I T_code <oluwagbotemioyeboade@gmail.com>
2023-03-18 23:01:02 +0100

    Accessing list in arrays of array

---
+++ Arrays of array.js
@@ -0,0 +1,11 @@
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData = myArray[2][1];
+/*The myData variable is assigned 
+the value 8,by bracket notation of accesing array.The nestedarray index is first acessed which is 2 then the index of the value 8
+is acessed which is [1]*/
