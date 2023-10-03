b998274656e1386e7b6c969a47348340935317d6
I T_code <oluwagbotemioyeboade@gmail.com>
2023-10-03 23:25:36 +0200

    Wordcounter l.js
    Each input value is gotten by their id.
    The field set value lenght is gotten by the use of length() method .

---
+++ wordcounter.js
@@ -0,0 +1,13 @@
+console.log('Hello World!');
+fieldset = document.getElementById("fieldset");
+counter = document.getElementById("counter");
+fieldset.oninput = function count(){
+let  input = fieldset.value;
+ result = input.length  ;
+ counter.value = result;}
+
+counter.innerHtml = document.write(value);
+a = "blsiehehehehehehehehhehehehehehheheheheueuueueueheheheheheheheheh"
+a = ["weh",24,24,35,64,35,43,"dash"]
+c = a.length
+b = document.write(c);
